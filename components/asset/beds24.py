7bc2b46c993ffd21056c7fcf3ce4d4aa395d39f5
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-15 10:43:48 +0600

    scrap beds24 room data

--- components/asset/beds24.py
+++ components/asset/beds24.py
@@ -1,11 +1,15 @@
 import os
+import ast
 import requests
 from dotenv import load_dotenv
 from langchain.tools import tool
 
 load_dotenv()
-@tool
+
 def GetRoomContent():
+    """
+    Get property description from Beds24
+    """
     url = "https://www.beds24.com/api/json/getPropertyContent"
 
     payload = {
@@ -19,3 +23,38 @@ def GetRoomContent():
     response = requests.post(url, json=payload)
 
     return response.json()
+
+
+# @tool
+def GetRoomInformation():
+    """
+    Get property description from Beds24
+    """
+
+    data = GetRoomContent()
+
+    room_data = data['getPropertyContent'][0]['roomIds']
+
+    # print(room_data)
+    room_ids = []
+    for k, v in room_data.items():
+        room_ids.append(int(k))
+
+    # print(room_ids)
+
+        
+    url = "https://www.beds24.com/api/json/getPropertyContent"
+
+    payload = {
+        "authentication": {
+            "apiKey": os.environ.get("beds24_apiKey"),
+            "propKey": os.environ.get("propKey")
+        },
+        "roomIds": room_ids
+    }
+
+    response = requests.post(url, json=payload)
+    response = response.json()
+    print(response)
+    response = response['getPropertyContent'][0]['roomIds']
+    return response
--- test.py
+++ test.py
@@ -60,24 +60,52 @@
 # else:
 #     print(f"Folder '{folder_path}' not found or is not a directory.")
 
-import requests
+# import requests
 
-url = "https://www.beds24.com/api/json/getPropertyContent"
+# url = "https://www.beds24.com/api/json/getPropertyContent"
 
-payload = {
-    "authentication": {
-        "apiKey": "h12j3123h123j28z",
-        "propKey": "Joybeach8754h6fdr5"
-    },
-    "texts": ["EN"]
-}
+# payload = {
+#     "authentication": {
+#         "apiKey": "h12j3123h123j28z",
+#         "propKey": "Joybeach8754h6fdr5"
+#     },
+#     "texts": ["EN"]
+# }
 
-headers = {
-    "Content-Type": "application/json"
-}
+# headers = {
+#     "Content-Type": "application/json"
+# }
 
-response = requests.post(url, json=payload, headers=headers)
+# response = requests.post(url, json=payload, headers=headers)
 
-print(response.status_code)
-print(response.json())
+# print(response.status_code)
+# print(response.json())
+
+import json
+from components.asset.beds24 import GetRoomContent, GetRoomInformation
+
+# # data = GetRoomContent()
+
+# # with open('data.json', 'w', encoding='utf-8') as f:
+# #     json.dump(data,f, indent=4)
+# with open('data.json', 'r' ,encoding='utf-8') as f:
+
+#     data = json.load(f)
+
+
+# room_data = data['getPropertyContent'][0]['roomIds']
+
+# print(room_data)
+# room_ids = []
+# for k, v in room_data.items():
+#     room_ids.append(int(k))
+
+# print(room_ids)
+
+room_info = GetRoomInformation()
+
+
+print(room_info)
+# with open('room_data.json', 'w', encoding='utf-8') as f:
+#     json.dump(room_info['getPropertyContent'][0]['roomIds'], f, indent=4)
 
