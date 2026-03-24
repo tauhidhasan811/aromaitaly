8cfb1fa99a89644ab7493d00e0a5fd702821e899
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-24 10:47:37 +0600

    successfully extract v2 data

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import os
 import json
 import requests
+from components.hyperparms import params
 
+def flatten_feature_codes(feature_codes):
+    flat_features = []
+
+    for item in feature_codes:
+        if isinstance(item, list):
+            flat_features.extend(item)
+        else:
+            flat_features.append(item)
+
+    return flat_features
 
 
 
@@ -26,7 +37,7 @@ FIELD_MAP = {
             "state": "state/location",
             "postcode": "postcode/location",
         }
-data = []
+proparty_info = []
 for prop in v2_json_data['data']:
 
 
@@ -41,11 +52,91 @@ for prop in v2_json_data['data']:
 
         item[new_field] = value
 
-    data.append(item)
+    proparty_info.append(item)
 
 with open("data/proparty_data.json", 'w', encoding='utf-8') as f:
+    json.dump(proparty_info, f, indent=4)
+
+
+missed_villaInfo = {
+            "proparty": proparty_info
+        }
+
+# properties = params["proparty_list"]
+fields = params["fields"]
+field_map = params["FIELD_MAP"]
+
+data = []
+
+# build grouped output keys using mapped field names
+field_lists = {
+    field_map.get(field, field): []
+    for field in fields
+    if field != "name"
+}
+
+
+
+for proper_data in v2_json_data['data']:
+    print("-" * 60)
+    print(" " * 25, proper_data['name'])
+    print("-" * 60)
+
+        
+        # villa_info, file = beds24.GetRoomInformation(property_name=proper)
+        
+        # print(missed_villaInfo)
+    for room_data in proper_data['roomTypes']:
+        print(room_data)
+        # for room_key, room_value in room_data.items():
+        #     print(room_key, room_value)
+        room = {"proparty": proper_data['name']}
+        
+
+        for field in fields:
+            # first get value using original source field name
+            value = room_data.get(field)
+
+            #  special handling for featureCodes
+            if field == "featureCodes" and value is not None:
+                value = flatten_feature_codes(value)
+
+            # then rename only the output key
+            new_field = field_map.get(field, field)
+            room[new_field] = value
+
+        data.append(room)
+
+        for field in fields:
+            if field == "name" or field == 'id':
+                continue
+            if room.get('id') == 337089 or room.get('id') == 642098:
+                continue
+
+            new_field = field_map.get(field, field)
+
+            field_lists[new_field].append({
+                    "proparty": proper_data['name'],
+                    "name": room.get(field_map.get("name", "name")),
+                    "roomId": room.get(field_map.get("id", "id")),
+                    new_field: room.get(new_field)
+                })
+    field_lists.update(missed_villaInfo)
+
+    data = [
+        {
+            "field": field,
+            "data": values
+        }
+        for field, values in field_lists.items()
+    ]
+
+with open("data/v2_beds24_room_data.json", 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=4)
 
+
+# with open("data/v2_beds24_data.json", 'w', encoding='utf-8') as f:
+#     json.dump(v2_json_data, f, indent=4)
 
 
 # with open("data/v2_beds24_data.json", 'w', encoding='utf-8') as f:
