690b02644368cd80f2a6758684d6fc23dfe4fa98
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-27 11:49:21 +0600

    process the prrrevious data

--- components/core/clean_text.py
+++ components/core/clean_text.py
@@ -15,3 +15,38 @@ def clean_previous_text(text):
     text = re.sub(r'\s+', ' ', text)
     
     return text.strip()
+
+
+def SelectedPreviousData(prev_info): 
+    split_data = prev_info.split('user_query')
+    selected_data = ""
+    minimun_stack = 2
+    current_stack = 0
+    for sp_data in reversed(split_data):
+        print('-' * 80)
+        sp_data = str(sp_data)
+        if len(sp_data) > 0:
+            sp_data = "user_query: "+ sp_data
+            sp_data = clean_previous_text(sp_data)
+        if len(selected_data) < 800:
+            selected_data += sp_data
+            print(f"if working && current content len: {len(selected_data)}")
+            # print(selected_data)
+            current_stack += 1
+        elif current_stack < minimun_stack:
+            selected_data = sp_data + selected_data
+            print(f"elif working && current content len: {len(selected_data)}")
+
+            current_stack += 1
+        else:
+            break
+        # print(sp_data)
+        # print(len(sp_data))
+        print('-' * 80)
+
+    print(selected_data)
+    print(current_stack)
+
+    # print(split_data[0])
+
+    return selected_data
